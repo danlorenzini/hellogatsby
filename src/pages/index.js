75719b6b0b3781cf7a01a852462a1162b30574d2
danlorenzini <danlorenzini@users.noreply.github.com>
2022-02-09 13:57:10 -0500

    Update index.js
    go

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,42 +81,42 @@ const badgeStyle = {
 // data
 const links = [
   {
-    text: "Tutorial",
+    text: "1. Tutorial",
     url: "https://www.gatsbyjs.com/docs/tutorial/",
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
     color: "#E95800",
   },
   {
-    text: "How to Guides",
+    text: "2. How to Guides",
     url: "https://www.gatsbyjs.com/docs/how-to/",
     description:
       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
     color: "#1099A8",
   },
   {
-    text: "Reference Guides",
+    text: "3. Reference Guides",
     url: "https://www.gatsbyjs.com/docs/reference/",
     description:
       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
     color: "#BC027F",
   },
   {
-    text: "Conceptual Guides",
+    text: "4. Conceptual Guides",
     url: "https://www.gatsbyjs.com/docs/conceptual/",
     description:
       "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
     color: "#0D96F2",
   },
   {
-    text: "Plugin Library",
+    text: "5. Plugin Library",
     url: "https://www.gatsbyjs.com/plugins",
     description:
       "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
     color: "#8EB814",
   },
   {
-    text: "Build and Host",
+    text: "6. Build and Host",
     url: "https://www.gatsbyjs.com/cloud",
     badge: true,
     description:
